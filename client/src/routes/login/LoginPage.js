c1701c544396f170dc7df8ecfc39e2aa57576880
Akriti Shukla <akritishukla00@gmail.com>
2023-07-09 05:20:53 +0530

    changes for login and register

--- client/src/routes/login/LoginPage.js
+++ client/src/routes/login/LoginPage.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import {React, useState} from 'react'
 import { Link } from 'react-router-dom'
-
+import './loginPageStyle.css'
 import '../../App.css'
 
+
 export default function SignInPage() {
+    const [role, setRole] = useState('');
+    const handleRoleChange = (event) => {
+        setRole(event.target.value);
+      };
     return (
-        <div className="text-center m-5-auto">
-            <h2>Sign in to us</h2>
-            <form action="/home">
+        <div className='all'>
+        <div className="text-center m-3-auto ">
+            <h2 className='hd4'>Sign In </h2>
+            <form action="/home" className='form3'>
+            <div>
+          <label className='labelData'>
+            Role:
+            <select value={role} onChange={handleRoleChange} >
+              <option value="">Select Role</option>
+              <option value="admin">Admin</option>
+              <option value="user">Coach</option>
+              <option value="user">Instructor</option>
+              
+            </select>
+          </label>
+        </div>
                 <p>
-                    <label>Username or email address</label><br/>
+                    <label className='labelData'>Username or email address</label><br/>
                     <input type="text" name="first_name" required />
                 </p>
                 <p>
-                    <label>Password</label>
+                    <label className='labelData'>Password</label>
                     <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
                     <br/>
                     <input type="password" name="password" required />
@@ -26,6 +44,7 @@ export default function SignInPage() {
                 <p>First time? <Link to="/register">Create an account</Link>.</p>
                 <p><Link to="/">Back to Homepage</Link>.</p>
             </footer>
+        </div>
         </div>
     )
 }
