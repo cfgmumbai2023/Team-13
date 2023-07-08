5e46bace052d5dfa067479f8f61b8969f97b4f35
Aakash Jain <aakashjainofficial@gmail.com>
2023-07-08 18:33:14 +0530

    setup-routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+
+import Authentication from "./routes/authentication/authentication";
+import Courses from "./routes/courses/courses";
+import Dashboard from "./routes/dashboard/dashboard";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Authentication />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/routes/authentication/authentication.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Authentication() {
+  return <div>Auth Page</div>;
+}
+
+export default Authentication;
