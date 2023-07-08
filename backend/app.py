6687611a2fab4ff7761a97c8ab93a5ddaf037153
Shashikant Dangi <u20cs072@coed.svnit.ac.in>
2023-07-09 00:57:03 +0530

    converted to json

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,36 @@
 from flask import Flask, jsonify
-from database import create_db_connection
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
 
-# Create an instance of the Flask class
+# Load the environment variables from .env file
+load_dotenv()
+
+# Access the environment variables
+DBpassword = os.getenv('DATABASE_PASSWORD')
+
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = DBpassword
+app.config['MYSQL_DB'] = 'jpmc_cfg'
 
-# Define a route and its corresponding function
-@app.route('/')
-def index():
-    connection = create_db_connection()
-    cursor = connection.cursor()
+mysql = MySQL(app)
 
-    cursor.execute('SELECT * FROM users')
-    data = cursor.fetchall()
+@app.route('/', methods=['GET'])
+def get_data():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM users")
+    rows = cur.fetchall()
+    cur.close()
+    employee =[]
+    content ={}
+    for result in rows:
+        content = {'userID': result[0], 'firstName': result[1], 'lastName': result[2], 'age': result[3], 'emailID': result[4], 'imageLink':result[5]}
+        employee.append(content)
+        content={}
+    return jsonify(employee)
 
-    cursor.close()
-    connection.close()
-
-    return jsonify(data)
-
-# Run the Flask application
 if __name__ == '__main__':
     app.run()
