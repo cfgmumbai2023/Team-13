f8098c3514cededfcc431a034669f5330ad3d473
Aakash Jain <aakashjainofficial@gmail.com>
2023-07-08 19:06:18 +0530

    setup-dashboard-structure

--- client/src/routes/dashboard/dashboard.js
+++ client/src/routes/dashboard/dashboard.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-aside"></div>
+      <div className="dashboard-main"></div>
+    </div>
+  );
 }
+
+
+
+
 
 export default Dashboard;
