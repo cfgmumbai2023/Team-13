80daad5271eb850a036773d43709c47094fd15bf
Aakash Jain <aakashjainofficial@gmail.com>
2023-07-09 07:41:07 +0530

    add rest api integration

--- client/src/routes/login/LoginPage.js
+++ client/src/routes/login/LoginPage.js
@@ -1,50 +1,95 @@
-import {React, useState} from 'react'
-import { Link } from 'react-router-dom'
-import './loginPageStyle.css'
-import '../../App.css'
+import { React, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
+import "./loginPageStyle.css";
+import "../../App.css";
 
-export default function SignInPage() {
-    const [role, setRole] = useState('');
-    const handleRoleChange = (event) => {
-        setRole(event.target.value);
-      };
-    return (
-        <div className='all'>
-        <div className="text-center m-3-auto ">
-            <h2 className='hd4'>Sign In </h2>
-            <form action="/home" className='form3'>
-            <div>
-          <label className='labelData'>
-            Role:
-            <select value={role} onChange={handleRoleChange} >
-              <option value="">Select Role</option>
-              <option value="admin">Admin</option>
-              <option value="user">Coach</option>
-              <option value="user">Instructor</option>
-              
-            </select>
-          </label>
-        </div>
-                <p>
-                    <label className='labelData'>Username or email address</label><br/>
-                    <input type="text" name="first_name" required />
-                </p>
-                <p>
-                    <label className='labelData'>Password</label>
-                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                    <br/>
-                    <input type="password" name="password" required />
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Login</button>
-                </p>
-            </form>
-            <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
-        </div>
-        </div>
-    )
+export default function SignInPage(props) {
+  const [role, setRole] = useState("");
+  const [uname, setUname] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (event, setter) => {
+    const value = event.target.value;
+    setter(value);
+  };
+
+  useEffect(() => {
+    console.log(role);
+    console.log(uname);
+    console.log(password);
+  }, [role, uname, password]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:5000/api/login', {
+      firstName: uname,
+      lastName: password,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+
+  return (
+    <div className="all">
+      <div className="text-center m-3-auto ">
+        <h2 className="hd4">Sign In</h2>
+        <form action="" className="form3">
+          <div>
+            <label className="labelData">
+              Role:
+              <select value={role} onChange={(e) => handleChange(e, setRole)}>
+                <option value="">Select Role</option>
+                <option value="admin">Admin</option>
+                <option value="user">Coach</option>
+                <option value="user">Instructor</option>
+              </select>
+            </label>
+          </div>
+          <p>
+            <label className="labelData">Username or email address</label>
+            <br />
+            <input
+              type="text"
+              name="first_name"
+              required
+              onChange={(e) => handleChange(e, setUname)}
+            />
+          </p>
+          <p>
+            <label className="labelData">Password</label>
+            <Link to="/forget-password">
+              <label className="right-label">Forget password?</label>
+            </Link>
+            <br />
+            <input
+              type="password"
+              name="password"
+              required
+              onChange={(e) => handleChange(e, setPassword)}
+            />
+          </p>
+          <p>
+            <button id="sub_btn" type="submit" onClick={(e) => handleSubmit(e)}>
+              Login
+            </button>
+          </p>
+        </form>
+        <footer>
+          <p>
+            First time? <Link to="/register">Create an account</Link>.
+          </p>
+          <p>
+            <Link to="/">Back to Homepage</Link>.
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
 }
--- client/src/routes/register/RegisterPage.js
+++ client/src/routes/register/RegisterPage.js
@@ -1,24 +1,26 @@
-import './RegisterPageStyle.css'
-import '../../App.css'
+import "./RegisterPageStyle.css";
+import "../../App.css";
 
-import React, { useState } from 'react';
+import axios from "axios";
+
+import React, { useState } from "react";
 
 const RegistrationForm = () => {
   const [userData, setUserData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    address: '',
-    city: '',
-    country: '',
-    education: '',
+    name: "",
+    email: "",
+    password: "",
+    address: "",
+    city: "",
+    country: "",
+    education: "",
     pdfFile: null,
     jpegFile: null,
-    previousExperience: ''
+    previousExperience: "",
   });
 
   const handleChange = (e) => {
-    if (e.target.type === 'file') {
+    if (e.target.type === "file") {
       setUserData({ ...userData, [e.target.name]: e.target.files[0] });
     } else {
       setUserData({ ...userData, [e.target.name]: e.target.value });
@@ -27,200 +29,204 @@ const RegistrationForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Perform form submission or validation here
-    console.log(userData);
+
+    axios
+      .post("http://localhost:5000/api/register", {
+        firstName: "Fred",
+        lastName: "Flintstone",
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
-    <div className='text-center m-2-auto form2 all2'>
-      <h1 className='head1'>Registration Form</h1>
-      <form onSubmit={handleSubmit} className='form1'>
+    <div className="text-center m-2-auto form2 all2">
+      <h1 className="head1">Registration Form</h1>
+      <form onSubmit={handleSubmit} className="form1">
         <h3>Personal Information</h3>
-       <div className="information">
-       <select>
-              <option value="">Salutations : Ms.</option>
-              <option value="mr">Mr.</option>
-              <option value="mrs">Mrs.</option>
-              
-            </select>
-       <input
-          type="text"
-          name="name"
-          className='mb3'
-          placeholder="Name"
-          value={userData.name}
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={userData.email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="contact"
-          placeholder="Contact Number"
-          value={userData.contact}
-          onChange={handleChange}
-        />
-
-       </div>
+        <div className="information">
+          <select>
+            <option value="">Salutations : Ms.</option>
+            <option value="mr">Mr.</option>
+            <option value="mrs">Mrs.</option>
+          </select>
+          <input
+            type="text"
+            name="name"
+            className="mb3"
+            placeholder="Name"
+            value={userData.name}
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={userData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="contact"
+            placeholder="Contact Number"
+            value={userData.contact}
+            onChange={handleChange}
+          />
+        </div>
 
         <h3>Address Information</h3>
-        <div className='information'>
-        <input
-          type="text"
-          name="address"
-          placeholder="Address"
-          value={userData.address}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="city"
-          placeholder="City"
-          value={userData.city}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="country"
-          placeholder="Country"
-          value={userData.country}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="pinCode"
-          placeholder="Pin Code"
-          value={userData.pinCode}
-          onChange={handleChange}
-        />  
+        <div className="information">
+          <input
+            type="text"
+            name="address"
+            placeholder="Address"
+            value={userData.address}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            value={userData.city}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="country"
+            placeholder="Country"
+            value={userData.country}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="pinCode"
+            placeholder="Pin Code"
+            value={userData.pinCode}
+            onChange={handleChange}
+          />
         </div>
 
         <h3>Sport Proficiecy</h3>
-        <div className='information-sport'>
-        <label className='labelData'> Sport :</label>
-        <select >
-              <option value="">Select Sport</option>
-              <option value="admin">Football</option>
-              <option value="user">Cricket</option>
-              <option value="user">Basket-ball</option>
-              
-            </select>
+        <div className="information-sport">
+          <label className="labelData"> Sport :</label>
+          <select>
+            <option value="">Select Sport</option>
+            <option value="admin">Football</option>
+            <option value="user">Cricket</option>
+            <option value="user">Basket-ball</option>
+          </select>
 
-        <label className='labelData'> Age Group you're confortable in coaching :</label>
-        <select >
-              <option value="">Select Age Group : All</option>
-              <option value="admin">6-12</option>
-              <option value="user">13-16</option>
-              <option value="user">16-21</option>
-              
-            </select>
-            </div>    
+          <label className="labelData">
+            {" "}
+            Age Group you're confortable in coaching :
+          </label>
+          <select>
+            <option value="">Select Age Group : All</option>
+            <option value="admin">6-12</option>
+            <option value="user">13-16</option>
+            <option value="user">16-21</option>
+          </select>
+        </div>
 
         <h3>Educational Information</h3>
-       <div className='information'>
-       <label className='labelData'> Graduation : </label>
-       <input
-          type="text"
-          name="GraduationDegree"
-          placeholder="Graduation Degree"
-          value={userData.education}
-          onChange={handleChange}
-        />
+        <div className="information">
+          <label className="labelData"> Graduation : </label>
+          <input
+            type="text"
+            name="GraduationDegree"
+            placeholder="Graduation Degree"
+            value={userData.education}
+            onChange={handleChange}
+          />
 
-       <input
-          type="text"
-          name="collegeName"
-          placeholder="College Name"
-          value={userData.education}
-          onChange={handleChange}
-        />
+          <input
+            type="text"
+            name="collegeName"
+            placeholder="College Name"
+            value={userData.education}
+            onChange={handleChange}
+          />
 
-        
-        <input
-          type="text"
-          name="Percentage"
-          placeholder="Percentage"
-          value={userData.education}
-          onChange={handleChange}
-        />
+          <input
+            type="text"
+            name="Percentage"
+            placeholder="Percentage"
+            value={userData.education}
+            onChange={handleChange}
+          />
+        </div>
 
-       </div>
+        <div className="information">
+          <label className="labelData"> Intermediate : </label>
+          <input
+            type="text"
+            name="intermediate Board"
+            placeholder="Internship Board"
+            value={userData.education}
+            onChange={handleChange}
+          />
 
-       <div className='information'>
-       <label className='labelData'> Intermediate : </label>
-       <input
-          type="text"
-          name="intermediate Board"
-          placeholder="Internship Board"
-          value={userData.education}
-          onChange={handleChange}
-        />
+          <input
+            type="text"
+            name="SchoolName"
+            placeholder="School Name"
+            value={userData.education}
+            onChange={handleChange}
+          />
 
-       <input
-          type="text"
-          name="SchoolName"
-          placeholder="School Name"
-          value={userData.education}
-          onChange={handleChange}
-        />
+          <input
+            type="text"
+            name="Percentage"
+            placeholder="Percentage"
+            value={userData.education}
+            onChange={handleChange}
+          />
+        </div>
 
-        
-        <input
-          type="text"
-          name="Percentage"
-          placeholder="Percentage"
-          value={userData.education}
-          onChange={handleChange}
-        />
+        <div className="information">
+          <label className="labelData"> HighSchool : </label>
+          <input
+            type="text"
+            name="HighSchool"
+            placeholder="High School"
+            value={userData.education}
+            onChange={handleChange}
+          />
 
-       </div>
+          <input
+            type="text"
+            name="schoolName"
+            placeholder="School Name"
+            value={userData.education}
+            onChange={handleChange}
+          />
 
-       <div className='information'>
-       <label className='labelData'> HighSchool : </label>
-       <input
-          type="text"
-          name="HighSchool"
-          placeholder="High School"
-          value={userData.education}
-          onChange={handleChange}
-        />
-
-       <input
-          type="text"
-          name="schoolName"
-          placeholder="School Name"
-          value={userData.education}
-          onChange={handleChange}
-        />
-
-        
-        <input
-          type="text"
-          name="Percentage"
-          placeholder="Percentage"
-          value={userData.education}
-          onChange={handleChange}
-        />
-
-       </div>
-
+          <input
+            type="text"
+            name="Percentage"
+            placeholder="Percentage"
+            value={userData.education}
+            onChange={handleChange}
+          />
+        </div>
 
         <h3>Licence/Certification Upload</h3>
-       <div className='information'>
-       <label htmlFor="pdfFile" className='labelData'>PDF File:</label>
-        <input
-          type="file"
-          name="pdfFile"
-          accept=".pdf"
-          onChange={handleChange}
-        />
-       </div>
-{/* 
+        <div className="information">
+          <label htmlFor="pdfFile" className="labelData">
+            PDF File:
+          </label>
+          <input
+            type="file"
+            name="pdfFile"
+            accept=".pdf"
+            onChange={handleChange}
+          />
+        </div>
+        {/* 
         <label htmlFor="jpegFile">JPEG File:</label>
         <input
           type="file"
@@ -230,16 +236,18 @@ const RegistrationForm = () => {
         /> */}
 
         <h3>Previous Experience (If Any)</h3>
-        <div className='information'>
-        <textarea
-          name="previousExperience"
-          placeholder="Previous experience"
-          value={userData.previousExperience}
-          onChange={handleChange}
-        ></textarea>
+        <div className="information">
+          <textarea
+            name="previousExperience"
+            placeholder="Previous experience"
+            value={userData.previousExperience}
+            onChange={handleChange}
+          ></textarea>
         </div>
 
-        <button type="submit" className='button1'>Submit</button>
+        <button type="submit" className="button1">
+          Submit
+        </button>
       </form>
     </div>
   );
